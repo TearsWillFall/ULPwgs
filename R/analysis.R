4b7961eab0e8f63502bcb02400540aee1b96bf8b
Osvaldas Vainauskas <osvaldas.v@um.es>
2020-06-06 11:03:06 +0200

    fix formatting

--- R/analysis.R
+++ R/analysis.R
@@ -1,6 +1,6 @@
 
 
-options(scipen=999)
+
 
 #' Generate a quality control (QC) report from a fastaqc file
 #'
@@ -365,7 +365,7 @@ qc_metrics=function(bin_path="tools/samtools/samtools",bin_path2="tools/picard/b
 
 
 read_counter=function(bin_path="tools/hmmcopy_utils/bin/readCounter",win=500000,bam="",output_dir="",chr="chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY",verbose=FALSE){
-
+    options(scipen=999)
     sep="/"
 
     if(output_dir==""){
