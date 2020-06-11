d36d8eca57b592c09e4af53bab5ca3e518abdfd4
Osvaldas Vainauskas <osvaldas.v@um.es>
2020-06-11 12:31:59 +0200

    dir creation for trimming

--- R/analysis.R
+++ R/analysis.R
@@ -26,7 +26,7 @@ fastqc=function (bin_path="tools/FastQC/bin/fastqc",file_R1="",file_R2="",n_core
 
   if (!file_R2==""){
   sample_name=intersect_sample_name(file_path=file_R1,file_path2=file_R2)
-  output_dir=paste0(output_dir,sep,sample_name)
+  output_dir=paste0(output_dir,sep,sample_name,"-FastQC_report")
 
   if(!dir.exists(output_dir)){
     dir.create(output_dir)
@@ -83,22 +83,32 @@ trimming=function(bin_path="tools/skewer/skewer",file_R1="",file_R2="",xadapt=NA
   }
   if (!file_R2==""){
   sample_name=intersect_sample_name(file_path=file_R1,file_path2=file_R2)
-  output_dir=paste0(output_dir,sep,sample_name)
+  output_dir=paste0(output_dir,sep,sample_name,"-trimmed")
 
     if(verbose){
-      print(paste(func,"-z -l 35 -f sanger --quiet -o",output_dir,file_R1,file_R2))
+      print(paste(func,"-z -l 35 -f sanger --quiet -o",smaple_name,file_R1,file_R2))
     }
-    system(paste(func,"-z -l 35 -f sanger --quiet -o",output_dir,file_R1,file_R2))
+    system(paste(func,"-z -l 35 -f sanger --quiet -o",sample_name,file_R1,file_R2))
 
 
   }else{
     output_dir=paste0(output_dir,sep,sample_name)
 
       if(verbose){
-        print(paste(func,"-z -l 35 -f sanger --quiet -o",output_dir,file_R1))
+        print(paste(func,"-z -l 35 -f sanger --quiet -o",sample_name,file_R1))
       }
-      system(paste(func,"-z -l 35 -f sanger --quiet -o",output_dir,file_R1))
+      system(paste(func,"-z -l 35 -f sanger --quiet -o",sample_name,file_R1))
     }
+
+    if (!dir.exists(output_dir)){
+        dir.create(output_dir)
+    }
+    if(verbose){
+      print(paste("mv",paste0(sample_name,"-trimmed*"), output_dir))
+    }
+
+    system(paste("mv",paste0(sample_name,"-trimmed*"), output_dir)
+
   }
 
 
@@ -311,7 +321,7 @@ qc_metrics=function(bin_path="tools/samtools/samtools",bin_path2="tools/picard/b
 
     sample_name=get_sample_name(bam)
 
-    out_file=paste0(output_dir,sep,"alignQC")
+    out_file=paste0(output_dir,sep,"_alignQC")
     if (!dir.exists(out_file)){
         dir.create(out_file)
     }
@@ -439,7 +449,7 @@ ichorCNA=function(bin_path="tools/ichorCNA/scripts/runIchorCNA.R",sample_id="",w
       sample_id=get_sample_name(wig)
     }
 
-    out_file=paste0(output_dir,sep,"report")
+    out_file=paste0(output_dir,sep,"-CNAreport")
     if (!dir.exists(out_file)){
         dir.create(out_file)
     }
