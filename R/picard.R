4aa967db4a51aa26559fc0e789ae7d469930ee75
TearsWillFall <osvaldas.vainauskas@gmail.com>
2022-07-27 10:56:45 +0100

    Fix

--- R/picard.R
+++ R/picard.R
@@ -335,7 +335,7 @@ tg_summary_metrics_bam_picard=function(
 
 
   job_report[["steps"]][["parse_summary"]]<-parse_picard_metrics(
-    metrics=out_file,
+    summary=out_file,
     output_dir=out_file_dir,
     mode=mode,executor_id=task_id,
     output_name=get_file_name(bam),
