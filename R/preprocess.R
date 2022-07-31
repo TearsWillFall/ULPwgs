adc37696faac9f9b1ceb7042979f7563ff8a5325
TearsWillFall <osvaldas.vainauskas@gmail.com>
2022-07-31 09:45:12 +0100

    Fix

--- R/preprocess.R
+++ R/preprocess.R
@@ -210,7 +210,7 @@ for_id=function(seq_info,output_dir="",name="",
                                 })
                             }else{
                                 rdata_file=paste0(out_file_dir,"/",new_name,".RData")
-                                save(list=append(list(.GlobalEnv),list(environment())),file =  rdata_file)
+                                save(list=as.list(environment()),file =  rdata_file)
                                 process_sample(rdata= rdata_file)
                             }
                 }
