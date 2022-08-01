e7eb896759f4900f9b89e9063022b6fd03c7a255
TearsWillFall <osvaldas.vainauskas@gmail.com>
2022-08-01 15:35:52 +0100

    Fix

--- R/gatk.R
+++ R/gatk.R
@@ -299,7 +299,7 @@ generate_BQSR_gatk=function(
        out_file_dir2=set_dir(dir=out_file_dir,name="batch")
        batch_code=build_job_exec(job=job,time=time,ram=ram,threads=threads,
        output_dir=out_file_dir2,hold=hold)
-       exec_code=paste0("echo '. /etc/bashrc;",batch_config,";",exec_code,"'|",batch_code)
+       exec_code=paste0("echo '. $HOME/.bashrc;",batch_config,";",exec_code,"'|",batch_code)
   }
 
   if(verbose){
