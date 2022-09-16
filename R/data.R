47800a0d9e0d9b6706f0bf44e2795f14e5edd416
TearsWillFall <osvaldas.vainauskas@gmail.com>
2022-09-16 15:46:01 +0100

    Fix

--- R/data.R
+++ R/data.R
@@ -136,7 +136,7 @@ build_flowcell_id=function(flowcells=list(
       "H[A-Z,0-9]{4}DSXY$",
       "H[A-Z,0-9]{4}DRXX$",
       "H[A-Z,0-9]{4}DRX2$",
-      "H[A-Z,0-9]{4}DRXY$",
+      "H[A-Z,0-9]{4}DRXY$"
     )
   )
   ) {
