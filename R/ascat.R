ff51649a74a43bbd07b32ef4a5de2416d8b088ce
TearsWillFall <osvaldas.vainauskas@gmail.com>
2022-11-09 10:53:34 +0000

    Fix

--- R/ascat.R
+++ R/ascat.R
@@ -1,28 +1,129 @@
+#' Analyze a pair of tumour-normal samples using ASCAT
+#' 
+#' @param bin_allele_counter [REQUIRED] Path to allele_counter binary.
+#' @param tumour [REQUIRED] Path to tumour BAM file.
+#' @param normal [REQUIRED] Path to normal BAM file.
+#' @param patient_id [REQUIRED] Path to normal BAM file.
+#' @param panel_version [OPTIONAL] PCF Select panel version. Default V3.
+#' @param ref_dataset [OPTIONAL] Reference dataset for panel allele and loci selection. Default battenberg
+#' @param gender [OPTIONAL] Sample gender. Default XY.
+#' @param genome_version [OPTIONAL] Genome reference version. Default HG19.
+#' @param ascat_ref [OPTIONAL] List with default references.
+#' @param batch_config [OPTIONAL] List with default references.
+#' @param gamma [OPTIONAL] Gamma parameter. Default 1.
+#' @param penalty [OPTIONAL] Penalty parameter. Default 0.25.
+#' @param output_dir [OPTIONAL] Path to the output directory.
+#' @param threads [OPTIONAL] Number of threads to split the work. Default 4
+#' @param ram [OPTIONAL] RAM memory to asing to each thread. Default 4
+#' @param verbose [OPTIONAL] Enables progress messages. Default False.
+#' @param mode [REQUIRED] Where to parallelize. Default local. Options ["local","batch"]
+#' @param executor_id Task EXECUTOR ID. Default "recalCovariates"
+#' @param task_name Task name. Default "recalCovariates"
+#' @param time [OPTIONAL] If batch mode. Max run time per job. Default "48:0:0"
+#' @param update_time [OPTIONAL] If batch mode. Job update time in seconds. Default 60.
+#' @param wait [OPTIONAL] If batch mode wait for batch to finish. Default FALSE
+#' @param hold [OPTIONAL] HOld job until job is finished. Job ID. 
+#' @export
+
 
 call_ascat=function(
-    rdata=NULL,
-    selected=NULL,
-    output_name="",
-    output_dir=".",
-    verbose=FALSE,
-    batch_config=build_default_preprocess_config(),
-    threads=4,ram=4,mode="local",
-    executor_id=make_unique_id("hybridIchorCNA"),
-    task_name="hybridIchorCNA",time="48:0:0",
-    update_time=60,
-    wait=FALSE,hold=NULL
+  rdata=NULL,
+  selected=NULL,
+  bin_allele_counter=build_default_tool_binary_list()$bin_allele_counter,
+  tumour=NULL,
+  normal=NULL,
+  patient_id=NULL,
+  ref_dataset="battenberg",
+  gender="XY",
+  genome_version="HG19",
+  panel_version="V3",
+  output_dir=".",
+  gamma=1,
+  penalty=0.25,
+  verbose=FALSE,
+  ascat_ref=build_default_reference_list(),
+  batch_config=build_default_preprocess_config(),
+  threads=8,
+  ram=4,mode="local",
+  executor_id=make_unique_id("prepareASCAT"),
+  task_name="prepareASCAT",time="48:0:0",
+  update_time=60,
+  wait=FALSE,hold=NULL
 ){
+  
+  
+  if(!is.null(rdata)){
+    load(rdata)
+    if(!is.null(selected)){
+      tumour=tumours_list[selected]
 
-  job_reports=prepare_ascat()
-  ascat.bc = ascat.loadData(Tumor_LogR_file = "Tumor_LogR.txt", Tumor_BAF_file = "Tumor_BAF.txt", Germline_LogR_file = "Germline_LogR.txt", Germline_BAF_file = "Germline_BAF.txt", gender = 'XX', genomeVersion = "hg19")
-  ascat.plotRawData(ascat.bc, img.prefix = "Before_correction_")
-  ascat.bc = ascat.correctLogR(ascat.bc, GCcontentfile = "GC_file.txt", replictimingfile = "RT_file.txt")
-  ascat.plotRawData(ascat.bc, img.prefix = "After_correction_")
-  ascat.bc = ascat.aspcf(ascat.bc)
-  ascat.plotSegmentedData(ascat.bc)
-  ascat.output = ascat.runAscat(ascat.bc, gamma=1, write_segments = T)
-  QC = ascat.metrics(ascat.bc,ascat.output)
-  save(ascat.bc, ascat.output, QC, file = 'ASCAT_objects.Rdata')
+    }
+  }
+
+
+  argg <- as.list(environment())
+  task_id=make_unique_id(task_name)
+  out_file_dir=set_dir(dir=output_dir,name=paste0(patient_id,"/",get_file_name(tumour),"/ascat_reports"))
+  job=build_job(executor_id=executor_id,task=task_id)
+  
+  if(is.null(tumour)|is.null(normal)){
+    stop("A pair of tumour and normal BAM files is required")
+  }
+
+  jobs_report=build_job_report(
+    job_id=job,
+    executor_id=executor_id,
+    exec_code=list(), 
+    task_id=task_id,
+    input_args=argg,
+    out_file_dir=out_file_dir,
+    out_files=list(
+      )
+    )
+
+
+
+
+  jobs_report[["steps"]][["prepare_ascat"]]=prepare_ascat(
+    bin_allele_counter=bin_allele_counter,
+    tumour=tumour,
+    normal=normal,
+    ref_dataset=ref_dataset,
+    gender=gender,
+    genome_version=genome_version,
+    panel_version=panel_version,
+    output_dir=paste0(out_file_dir,"/prepare_ascat"),
+    verbose=verbose,
+    ascat_ref=ascat_ref,
+    batch_config=batch_config,
+    threads=threads,
+    ram=ram,mode=mode,
+    executor_id=task_id,
+    time=time,hold=hold
+  )
+
+  jobs_report[["steps"]][["process_ascat"]]=process_ascat(
+    tumour_log2=jobs_report[["steps"]][["prepare_ascat"]]$out_files$tumour_log2,
+    normal_log2=jobs_report[["steps"]][["prepare_ascat"]]$out_files$normal_log2,
+    tumour_baf=jobs_report[["steps"]][["prepare_ascat"]]$out_files$tumour_baf,
+    normal_baf=jobs_report[["steps"]][["prepare_ascat"]]$out_files$normal_baf,
+    ref_dataset=ref_dataset,
+    gender=gender,
+    genome_version=genome_version,
+    panel_version=panel_version,
+    gamma=gamma,
+    output_dir=paste0(out_file_dir,"/process_ascat"),
+    verbose=verbose,
+    ascat_ref=ascat_ref,
+    batch_config=natch_config,
+    threads=threads,
+    ram=ram,mode=mode,
+    executor_id=task_id,
+    time=time,
+    hold=hold
+  )
+
+  return(jobs_report)
 
 }
 
@@ -34,10 +135,9 @@ call_ascat=function(
 #' @param bin_allele_counter [REQUIRED] Path to allele_counter binary.
 #' @param tumour [REQUIRED] Path to tumour BAM file.
 #' @param normal [REQUIRED] Path to normal BAM file.
-#' @param version [OPTIONAL] PCF Select panel version. Default V3.
+#' @param panel_version [OPTIONAL] PCF Select panel version. Default V3.
 #' @param ref_dataset [OPTIONAL] Reference dataset for panel allele and loci selection. Default battenberg
 #' @param gender [OPTIONAL] Sample gender. Default XY.
-#' @param patient_id [OPTIONAL] Patient identifier. Default NULL
 #' @param genome_version [OPTIONAL] Genome reference version. Default HG19.
 #' @param ascat_ref [OPTIONAL] List with default references.
 #' @param batch_config [OPTIONAL] List with default references.
@@ -56,12 +156,9 @@ call_ascat=function(
 
 
 prepare_ascat=function(
-  rdata=NULL,
-  selected=NULL,
   bin_allele_counter=build_default_tool_binary_list()$bin_allele_counter,
   tumour=NULL,
   normal=NULL,
-  patient_id=NULL,
   ref_dataset="battenberg",
   gender="XY",
   genome_version="HG19",
@@ -78,28 +175,16 @@ prepare_ascat=function(
   wait=FALSE,hold=NULL
 ){
 
-
-if(!is.null(rdata)){
-    load(rdata)
-    if(!is.null(selected)){
-      region=region_list[selected]
-    }
-  }
-
-  if(is.null(tumour)|is.null(normal)){
-    stop("A pair of tumour and normal BAM files is required")
-  }
-
   
-  if(is.null(patient_id)){
-    stop("Patient ID argument is required")
-  }
-
 
   argg <- as.list(environment())
   task_id=make_unique_id(task_name)
-  out_file_dir=set_dir(dir=output_dir,name=paste0(patient_id,"/",get_file_name(tumour),"/",genome_version,"/",panel_version,"/",ref_dataset))
+  out_file_dir=set_dir(dir=output_dir)
   job=build_job(executor_id=executor_id,task=task_id)
+  
+  if(is.null(tumour)|is.null(normal)){
+    stop("A pair of tumour and normal BAM files is required")
+  }
   
 
   out_file_tumour_log2=paste0(get_file_name(tumour),".LogR.txt")
@@ -151,22 +236,175 @@ if(!is.null(rdata)){
   }
 
   job_report=build_job_report(
-  job_id=job,
-  executor_id=executor_id,
-  exec_code=exec_code, 
-  task_id=task_id,
-  input_args = argg,
-  out_file_dir=out_file_dir,
-  out_files=list(
-    tumour_log2=out_file_tumour_log2,
-    tumour_baf=out_file_tumour_baf,
-    normal_log2=out_file_normal_log2,
-    normal_baf=out_file_normal_baf)
+    job_id=job,
+    executor_id=executor_id,
+    exec_code=exec_code, 
+    task_id=task_id,
+    input_args = argg,
+    out_file_dir=out_file_dir,
+    out_files=list(
+      tumour_log2=paste0(out_file_dir,"/",out_file_tumour_log2),
+      tumour_baf=paste0(out_file_dir,"/",out_file_tumour_baf),
+      normal_log2=paste0(out_file_dir,"/",out_file_normal_log2),
+      normal_baf=paste0(out_file_dir,"/",out_file_normal_baf)
+      )
   )
 
   return(job_report)
 
 
+}
+
+
+
+
+#' Prepare ASCAT data from PCF select BAM files
+#' 
+#' @param tumour [REQUIRED] Path to tumour BAM file.
+#' @param normal [REQUIRED] Path to normal BAM file.
+#' @param version [OPTIONAL] PCF Select panel version. Default V3.
+#' @param ref_dataset [OPTIONAL] Reference dataset for panel allele and loci selection. Default battenberg
+#' @param gender [OPTIONAL] Sample gender. Default XY.
+#' @param genome_version [OPTIONAL] Genome reference version. Default HG19.
+#' @param ascat_ref [OPTIONAL] List with default references.
+#' @param gamma [OPTIONAL] Gamma parameter. Default 1.
+#' @param penalty [OPTIONAL] Penalty parameter. Default 0.25.
+#' @param batch_config [OPTIONAL] List with default references.
+#' @param output_dir [OPTIONAL] Path to the output directory.
+#' @param threads [OPTIONAL] Number of threads to split the work. Default 4
+#' @param ram [OPTIONAL] RAM memory to asing to each thread. Default 4
+#' @param verbose [OPTIONAL] Enables progress messages. Default False.
+#' @param mode [REQUIRED] Where to parallelize. Default local. Options ["local","batch"]
+#' @param executor_id Task EXECUTOR ID. Default "recalCovariates"
+#' @param task_name Task name. Default "recalCovariates"
+#' @param time [OPTIONAL] If batch mode. Max run time per job. Default "48:0:0"
+#' @param update_time [OPTIONAL] If batch mode. Job update time in seconds. Default 60.
+#' @param wait [OPTIONAL] If batch mode wait for batch to finish. Default FALSE
+#' @param hold [OPTIONAL] HOld job until job is finished. Job ID. 
+#' @export
+
+
+process_ascat=function(
+  tumour_log2=NULL,
+  normal_log2=NULL,
+  tumour_baf=NULL,
+  normal_baf=NULL,
+  ref_dataset="battenberg",
+  gender="XY",
+  genome_version="HG19",
+  gamma=1,
+  penalty=0.25,
+  panel_version="V3",
+  output_dir=".",
+  verbose=FALSE,
+  ascat_ref=build_default_reference_list(),
+  batch_config=build_default_preprocess_config(),
+  threads=8,
+  ram=4,mode="local",
+  executor_id=make_unique_id("prepareASCAT"),
+  task_name="prepareASCAT",time="48:0:0",
+  update_time=60,
+  wait=FALSE,hold=NULL
+){
+
+
+  if(is.null(tumour_log2)|is.null(normal_log2)){
+    stop("A pair of tumour and normal LogR files are required. These can be prepared using the prepare_ascat function")
+  }
+
+  if(is.null(tumour_baf)|is.null(normal_baf)){
+    stop("A pair of tumour and normal BAF files are required. These can be prepared using the prepare_ascat function")
+  }
+
+
+
+  tumour_id=get_file_name(tumour_log2)
+  normal_id=get_file_name(normal_log2)
+
+
+
+  argg <- as.list(environment())
+  task_id=make_unique_id(task_name)
+  out_file_dir=set_dir(dir=output_dir)
+  job=build_job(executor_id=executor_id,task=task_id)
+  
+
+  gc=ascat_ref[[toupper(genome_version)]]$panel[[paste0("PCF_",toupper(panel_version))]]$ascat[[ref_dataset]]$gc
+  rt=ascat_ref[[toupper(genome_version)]]$panel[[paste0("PCF_",toupper(panel_version))]]$ascat[[ref_dataset]]$rt
+
+  
+  rdata=paste0(tumour_id,'.ASCAT_objects.Rdata')
+
+  exec_code=paste0("Rscript -e \"","setwd(\\\"",
+            out_file_dir,"\\\");library(ASCAT);",
+            "ascat.bc = ASCAT::ascat.loadData(Tumor_LogR_file =\\\"",tumour_log2,"\\\"",
+            ",Tumor_BAF_file =\\\"",tumour_baf,"\\\"",
+            ",Normal_BAF_file =\\\"",normal_baf,"\\\"",
+            ",Normal_LogR_file =\\\"",normal_log2,"\\\"",
+            ",gender=\\\"",toupper(gender),"\\\"",
+            ",genomeVersion=\\\"",tolower(genome_version),"\\\");",
+            "ASCAT::ascat.plotRawData(ascat.bc,img.prefix = \\\"Before_correction_\\\");",
+            "ascat.bc = ASCAT::ascat.correctLogR(ascat.bc, GCcontentfile =\\\"",gc,"\\\"",
+            ",replictimingfile =\\\"",rt,"\\\");",
+            "ASCAT::ascat.plotRawData(ascat.bc,img.prefix = \\\"After_correction_\\\");",
+            "ascat.bc = ASCAT::ascat.aspcf(ascat.bc,penalty=,",penalty,");",
+            "ascat.output = ASCAT::ascat.runAscat(ascat.bc, gamma=",gamma,", write_segments = T);",
+            "ASCAT::ascat.plotSegmentedData(ascat.bc);",
+            "QC = ASCAT::ascat.metrics(ascat.bc,ascat.output);",
+            "save(ascat.bc, ascat.output, QC, file =\\\"",rdata,"\\\")\""
+  )
+
+
+ if(mode=="batch"){
+       out_file_dir2=set_dir(dir=out_file_dir,name="batch")
+       batch_code=build_job_exec(job=job,time=time,ram=ram,threads=threads,
+       output_dir=out_file_dir2,hold=hold)
+       exec_code=paste0("echo '. $HOME/.bashrc;",batch_config,";",exec_code,"'|",batch_code)
+  }
+
+
+  if(verbose){
+  print_verbose(job=job,arg=argg,exec_code=exec_code)
+  }
+
+  error=execute_job(exec_code=exec_code)
+  
+  if(error!=0){
+      stop("gatk failed to run due to unknown error.
+      Check std error for more information.")
+  }
+
+  job_report=build_job_report(
+    job_id=job,
+    executor_id=executor_id,
+    exec_code=exec_code, 
+    task_id=task_id,
+    input_args = argg,
+    out_file_dir=out_file_dir,
+    out_files=list(
+        plots=list(
+          before_corr_normal=paste0(out_file_dir,"/Before_correction_",tumour_id,".germline.png"),
+          after_corr_normal=paste0(out_file_dir,"/After_correction_",tumour_id,".germline.png"),
+          before_corr_tumour=paste0(out_file_dir,"/Before_correction_",tumour_id,".tumour.png"),
+          after_corr_tumour=paste0(out_file_dir,"/After_correction_",tumour_id,".tumour.png"),
+          aspcf=paste0(out_file_dir,"/",tumour_id,".ASPCF.png"),
+          ascat_profile=paste0(out_file_dir,"/",tumour_id,".ASCATfprofile.png"),
+          raw_profile=paste0(out_file_dir,"/",tumour_id,".rawprofile.png"),
+          sunrise=paste0(out_file_dir,"/",tumour_id,".sunrise.png")
+        ),
+        data=list(
+          rdata=paste0(out_file_dir,"/",rdata),
+          tumour_log2_pcfed=paste0(out_file_dir,"/",tumour_id,".LogR.PCFed.txt"),
+          tumour_baf_pcfed=paste0(out_file_dir,"/",tumour_id,".BAF.PCFed.txt"),
+          normal_log2_pcfed=paste0(out_file_dir,"/",normal_id,".LogR.PCFed.txt"),
+          normal_baf_pcfed=paste0(out_file_dir,"/",normal_id,".BAF.PCFed.txt"),
+          segments=paste0(out_file_dir,"/",tumour_id,".segments.txt"),
+          segments_raw=paste0(out_file_dir,"/",tumour_id,".segments_raw.txt")
+        )
+      )
+  )
+
+  return(job_report)
 }
 
 
