bb071a1c301e5c48b0b5063f972ea01ffba0f421
TearsWillFall <osvaldas.vainauskas@gmail.com>
2022-12-01 20:06:14 +0000

    Fix

--- R/vcf.R
+++ R/vcf.R
@@ -285,6 +285,129 @@ add_indel_af_strelka_vcf=function(
 
 
 
+#' Add structural variant AF information to Strelka VCF file
+#' This function reads and modifies Strelka produced VCF files to 
+#' add an entry for AF within the VCF file
+#' 
+#' @param bin_bgzip Path to bgzip executable.
+#' @param bin_tabix Path to TABIX executable.
+#' @param vcf Path to VCF file
+#' @param compress Compress VCF file. Default TRUE.
+#' @param index Index VCF file. Default TRUE.
+#' @param index_format VCF index format. Default tbi. Options [tbi,cbi].
+#' @param bgzip_index Create BGZIP index for compressed file. Default FALSE
+#' @param output_dir Path to the output directory.
+#' @param clean Remove input VCF after completion. Default FALSE.
+#' @param verbose Enables progress messages. Default False.
+#' @param executor_id Task EXECUTOR ID. Default "gatherBQSR"
+#' @param task_name Task name. Default "gatherBQSR"
+#' @param mode [REQUIRED] Where to parallelize. Default local. Options ["local","batch"]
+#' @param time [OPTIONAL] If batch mode. Max run time per job. Default "48:0:0"
+#' @param threads Number of threads to split the work. Default 3
+#' @param ram [OPTIONAL] If batch mode. RAM memory in GB per job. Default 1
+#' @param update_time [OPTIONAL] If batch mode. Show job updates every update time. Default 60
+#' @param wait [OPTIONAL] If batch mode wait for batch to finish. Default FALSE
+#' @param hold [OPTIONAL] HOld job until job is finished. Job ID. 
+#' @export
+
+
+
+add_sv_af_strelka_vcf=function(
+  bin_bgzip=build_default_tool_binary_list()$bin_bgzip,
+  bin_tabix=build_default_tool_binary_list()$bin_tabix,
+  vcf,overwrite=FALSE,
+  compress=TRUE,index=TRUE,
+  index_format="tbi",bgzip_index=FALSE,
+  clean=TRUE,verbose=FALSE, 
+  executor_id=make_unique_id("addAFindelStrelka"),
+  task_name="addAFindelStrelka",
+  batch_config=build_default_preprocess_config(),
+  mode="local",time="48:0:0",
+  threads=1,ram=4,update_time=60,
+  wait=FALSE,hold=NULL){
+    
+    argg <- as.list(environment())
+    task_id=make_unique_id(task_name)
+    out_file=paste0(get_file_name(vcf),".indel")
+    out_file_dir=dirname(vcf)
+    job=build_job(executor_id=executor_id,task_id=task_id)
+
+
+    job_report=build_job_report(
+      job_id=job,
+      executor_id=executor_id,
+      exec_code=list(),
+      task_id=task_id,
+      input_args = argg,
+      out_file_dir=out_file_dir,
+      out_files=list()
+    )
+
+
+    vcf_dat=read_vcf(vcf)
+    vcf_dat$body=vcf_dat$body %>% unnest_vcf_body()
+    vcf_dat$body=vcf_dat$body %>% dplyr::group_by_at(dplyr::vars(-VALUE,-FORMAT)) %>% 
+    dplyr::group_modify(~dplyr::add_row(.x,FORMAT="AFP")) %>% 
+    dplyr::group_modify(~dplyr::add_row(.x,FORMAT="AFS"))
+    ##Extract Tier1 read information for REF and ALT
+    vcf_dat$body=vcf_dat$body %>% dplyr::mutate(
+      PREF=tryCatch({strsplit(VALUE[FORMAT=="PR"],split=",")[[1]][1]},error=function(e){NA}),
+      PALT=tryCatch({strsplit(VALUE[FORMAT=="PR"],split=",")[[1]][2]},error=function(e){NA}),
+      SREF=tryCatch({strsplit(VALUE[FORMAT=="SR"],split=",")[[1]][1]},error=function(e){NA}),
+      SALT=tryCatch({strsplit(VALUE[FORMAT=="SR"],split=",")[[1]][2]},error=function(e){NA})) %>%
+      dplyr::mutate(VALUE=ifelse(FORMAT=="AFP",
+      as.numeric(PALT)/(as.numeric(PREF)+as.numeric(PALT)),VALUE))%>% 
+      dplyr::mutate(VALUE=ifelse(FORMAT=="AFS",
+      as.numeric(SALT)/(as.numeric(SREF)+as.numeric(SALT)),VALUE)) %>% 
+      dplyr::select(-c(PALT,PREF,SALT,SREF))
+    vcf_dat$body=vcf_dat$body %>% 
+    dplyr::mutate(VALUE=ifelse(is.na(VALUE),"",VALUE)) %>% nest_vcf_body()
+
+    
+    add_afp_descriptor<-function(){
+        list(Number="1",Type="Float",Description="\"Variant allelic frequency by paired reads spanning the region.\"")
+    }
+
+    add_afs_descriptor<-function(){
+        list(Number="1",Type="Float",Description="\"Variant allelic frequency by split-reads spanning the region.\"")
+    }
+
+    vcf_dat$descriptors$FORMAT[["AFP"]]<-add_afp_descriptor()
+    vcf_dat$descriptors$FORMAT[["AFS"]]<-add_afs_descriptor()
+    
+    if(!overwrite){
+      out_file=paste0(out_file,".af")
+    }
+
+    job_report[["steps"]][["writeVCF"]]<-write_vcf(
+      vcf=vcf_dat,
+      output_name=out_file,
+      output_dir=out_file_dir,
+      bin_bgzip=bin_bgzip,
+      bin_tabix=bin_tabix,
+      compress=compress,
+      index=index,index_format=index_format,
+      bgzip_index=bgzip_index,
+      clean=clean,verbose=verbose,mode=mode,
+      batch_config=batch_config,
+      time=time,threads=threads,
+      ram=ram,hold=hold
+    )
+  
+    return(job_report)
+}
+
+
+
+
+
+
+
+
+
+
+
+
 #' Add AF information to Strelka VCF file
 #' This function reads and modifies Strelka produced VCF files to 
 #' add an entry for AF within the VCF file
@@ -317,6 +440,7 @@ add_indel_af_strelka_vcf=function(
       bin_tabix=build_default_tool_binary_list()$bin_tabix,
       vcf_snv=NULL,
       vcf_indel=NULL,
+      vcf_sv=NULL,
       verbose=FALSE, 
       executor_id=make_unique_id("addAFStrelka"),
       task_name="addAFStrelka",
@@ -338,25 +462,26 @@ add_indel_af_strelka_vcf=function(
             out_files=list(
               )
             )
-
+          if(!is.null(vcf_snv)){
+              jobs_report[["steps"]][["annotateAFsnvStrelka"]]<-add_snv_af_strelka_vcf(
+                  bin_bgzip=bin_bgzip,
+                  bin_tabix=bin_tabix,
+                  vcf=vcf_snv,
+                  compress=TRUE,index=TRUE,
+                  index_format="tbi",bgzip_index=FALSE,
+                  clean=TRUE,verbose=verbose, 
+                  executor_id=task_id,
+                  batch_config=batch_config,
+                  mode=mode,time=time,
+                  threads=1,ram=1,
+                  hold=hold
+              )
+          }
             
-          jobs_report[["steps"]][["annotateAFsnvStrelka"]]<-add_snv_af_strelka_vcf(
-              bin_bgzip=bin_bgzip,
-              bin_tabix=bin_tabix,
-              vcf=vcf_snv,
-              compress=TRUE,index=TRUE,
-              index_format="tbi",bgzip_index=FALSE,
-              clean=TRUE,verbose=verbose, 
-              executor_id=task_id,
-              batch_config=batch_config,
-              mode=mode,time=time,
-              threads=1,ram=1,
-              hold=hold
-          )
+        
 
-
-
-        jobs_report[["steps"]][["annotateAFindelStrelka"]]<-add_indel_af_strelka_vcf(
+        if(!is.null(vcf_indel)){
+          jobs_report[["steps"]][["annotateAFindelStrelka"]]<-add_indel_af_strelka_vcf(
             bin_bgzip=bin_bgzip,
             bin_tabix=bin_tabix,
             vcf=vcf_indel,
@@ -370,11 +495,31 @@ add_indel_af_strelka_vcf=function(
             threads=threads,ram=ram,
             hold=hold
         )
+        }
+
+     if(!is.null(vcf_sv)){
+        
+        jobs_report[["steps"]][["annotateAFsvStrelka"]]<-add_sv_af_strelka_vcf(
+            bin_bgzip=bin_bgzip,
+            bin_tabix=bin_tabix,
+            vcf=vcf_indel,
+            compress=TRUE,index=TRUE,
+            index_format="tbi",
+            bgzip_index=FALSE,
+            clean=TRUE,verbose=verbose, 
+            executor_id=task_id,
+            batch_config=batch_config,
+            mode=mode,time=time,
+            threads=threads,ram=ram,
+            hold=hold
+        )
+     }
+
 
     jobs_report$out_files=list(
         vcf_snv=unlist_lvl( jobs_report[["steps"]][["annotateAFsnvStrelka"]],var="compressed_vcf"),
-        vcf_indel=unlist_lvl(jobs_report[["steps"]][["annotateAFindelStrelka"]],var="compressed_vcf")
-
+        vcf_indel=unlist_lvl(jobs_report[["steps"]][["annotateAFindelStrelka"]],var="compressed_vcf"),
+        vcf_sv=unlist_lvl(jobs_report[["steps"]][["annotateAFindelStrelka"]],var="compressed_vcf")
     )
 
   return(jobs_report)
@@ -788,10 +933,31 @@ extract_pass_variants_strelks_vcf=function(
           hold=hold
        )
 
+    jobs_report[["steps"]][["extractPASSindelVCF"]]<-
+        variants_by_filters_vcf(
+          bin_bgzip=bin_bgzip,
+          bin_tabix=bin_tabix,
+          vcf=vcf_indel,
+          filters="PASS",
+          output_name="somaticSV",
+          exclusive=TRUE,
+          compress=TRUE,
+          clean=TRUE,
+          output_dir=out_file_dir,
+          verbose=verbose,
+          batch_config=batch_config,
+          threads=1,ram=1,mode=mode,
+          executor_id=task_id,
+          time=time,
+          hold=hold
+       )
+
+
 
     jobs_report$out_files=list(
         vcf_snv=unlist_lvl( jobs_report[["steps"]][["extractPASSsnvVCF"]],var="compressed_vcf"),
-        vcf_indel=unlist_lvl(jobs_report[["steps"]][["extractPASSindelVCF"]],var="compressed_vcf")
+        vcf_indel=unlist_lvl(jobs_report[["steps"]][["extractPASSindelVCF"]],var="compressed_vcf"),
+        vcf_sv=unlist_lvl(jobs_report[["steps"]][["extractPASSsvVCF"]],var="compressed_vcf")
     )
 
 
@@ -947,128 +1113,6 @@ nest_vcf_body=function(vcf_body,full=FALSE){
    vcf_body=vcf_body %>% tidyr::nest(FILTER=FILTER)
   }
   return(vcf_body)
-}
-
-
-
-
-#' Add sv AF information to Strelka VCF file
-#' This function reads and modifies Strelka produced VCF files to 
-#' add an entry for AF within the VCF file
-#' 
-#' @param bin_bgzip Path to bgzip executable.
-#' @param bin_tabix Path to TABIX executable.
-#' @param vcf Path to VCF file
-#' @param compress Compress VCF file. Default TRUE.
-#' @param index Index VCF file. Default TRUE.
-#' @param index_format VCF index format. Default tbi. Options [tbi,cbi].
-#' @param bgzip_index Create BGZIP index for compressed file. Default FALSE
-#' @param output_dir Path to the output directory.
-#' @param clean Remove input VCF after completion. Default FALSE.
-#' @param verbose Enables progress messages. Default False.
-#' @param executor_id Task EXECUTOR ID. Default "gatherBQSR"
-#' @param task_name Task name. Default "gatherBQSR"
-#' @param mode [REQUIRED] Where to parallelize. Default local. Options ["local","batch"]
-#' @param time [OPTIONAL] If batch mode. Max run time per job. Default "48:0:0"
-#' @param threads Number of threads to split the work. Default 3
-#' @param ram [OPTIONAL] If batch mode. RAM memory in GB per job. Default 1
-#' @param update_time [OPTIONAL] If batch mode. Show job updates every update time. Default 60
-#' @param wait [OPTIONAL] If batch mode wait for batch to finish. Default FALSE
-#' @param hold [OPTIONAL] HOld job until job is finished. Job ID. 
-#' @export
-
-
-add_sv_af_strelka_vcf=function(
-  bin_bgzip=build_default_tool_binary_list()$bin_bgzip,
-  bin_tabix=build_default_tool_binary_list()$bin_tabix,
-  vcf,overwrite=FALSE,
-  compress=TRUE,index=TRUE,
-  index_format="tbi",bgzip_index=FALSE,
-  clean=TRUE,verbose=FALSE, 
-  executor_id=make_unique_id("addAFsvStrelka"),
-  task_name="addAFsvStrelka",
-  batch_config=build_default_preprocess_config(),
-  mode="local",time="48:0:0",
-  threads=1,ram=4,update_time=60,
-  wait=FALSE,hold=NULL){
-    
-    argg <- as.list(environment())
-    task_id=make_unique_id(task_name)
-    out_file=paste0(get_file_name(vcf),".sv")
-    out_file_dir=dirname(vcf)
-    job=build_job(executor_id=executor_id,task_id=task_id)
-
-
-    job_report=build_job_report(
-      job_id=job,
-      executor_id=executor_id,
-      exec_code=list(),
-      task_id=task_id,
-      input_args = argg,
-      out_file_dir=out_file_dir,
-      out_files=list()
-    )
-
-    
-    vcf_dat=read_vcf(vcf)
-
-    vcf_dat$body=vcf_dat$body %>% unnest_vcf_body() %>% 
-      dplyr::group_by_at(dplyr::vars(-VALUE,-FORMAT)) %>% 
-      dplyr::group_modify(~dplyr::add_row(.x,FORMAT="AF")
-    )
-   
-   
-    ##Extract Tier1 read information for REF and ALT
-
-
-    vcf_dat$body=vcf_dat$body %>% dplyr::mutate(
-      UREFPR=tryCatch({strsplit(VALUE[FORMAT=="PR"],split=",")[[1]][1]},error=function(e){NA}),
-      UALTPR=tryCatch({strsplit(VALUE[FORMAT=="PR"],split=",")[[1]][2]},error=function(e){NA}),
-      UREFSR=tryCatch({strsplit(VALUE[FORMAT=="SR"],split=",")[[1]][1]},error=function(e){NA}),
-      UALTSR=tryCatch({strsplit(VALUE[FORMAT=="SR"],split=",")[[1]][2]},error=function(e){NA})
-    ) %>%
-      dplyr::mutate(
-        VALUE=ifelse(
-          FORMAT=="AF",list(
-            as.numeric(UALTPR)/(as.numeric(UREFPR)+as.numeric(UALTPR)),
-            as.numeric(UALTSR)/(as.numeric(UREFSR)+as.numeric(UALTSR)))
-        ,VALUE))%>% dplyr::select(-c(UALTPR,UREFPR,UALTSR,UREFSR))
-
-    vcf_dat$body=vcf_dat$body %>% 
-    dplyr::mutate(VALUE=ifelse(is.na(VALUE),"",VALUE))%>%
-    nest_vcf_body()
-    
-
-    add_af_descriptor<-function(){
-        list(
-          Number="2",Type="Float",
-          Description="\"Variant allelic frequency for supporting  reads and split-reads in the listed order\"")
-    }
-
-    vcf_dat$descriptors$FORMAT[["AF"]]<-add_af_descriptor()
-
-
-
-    if(!overwrite){
-      out_file=paste0(out_file,".af")
-    }
-
-    job_report[["steps"]][["writeVCF"]]<-write_vcf(
-      vcf=vcf_dat,
-      output_name=out_file,
-      output_dir=out_file_dir,
-      bin_bgzip=bin_bgzip,
-      bin_tabix=bin_tabix,
-      compress=compress,
-      index=index,index_format=index_format,
-      bgzip_index=bgzip_index,
-      clean=clean,verbose=verbose,mode=mode,
-      batch_config=batch_config,
-      time=time,threads=threads,
-      ram=ram,hold=hold
-    )
-  
-    return(job_report)
 }
 
 
