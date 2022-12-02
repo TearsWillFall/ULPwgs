c0c045549bc37f0455671c5fafae147fa44fec26
TearsWillFall <osvaldas.vainauskas@gmail.com>
2022-12-02 16:15:54 +0000

    Fix

--- R/vcf.R
+++ R/vcf.R
@@ -1068,8 +1068,8 @@ tabulate_vcf=function(
   task_id=make_unique_id(task_name)
   out_file_dir=set_dir(dir=output_dir)
   job=build_job(executor_id=executor_id,task_id=task_id)
-  func_name=rlang::call_name()
-  func_nspace=rlang::call_namespace()
+  func_name=rlang::call_name(rlang::current_fn())
+  func_nspace=rlang::call_namespace(rlang::current_fn())
 
   jobs_report=build_job_report(
     job_id=job,
