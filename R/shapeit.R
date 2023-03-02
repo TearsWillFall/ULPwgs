3a506cfadaa61b687168e70e1fd2af33643568fa
TearsWillFall <osvaldas.vainauskas@gmail.com>
2023-03-02 15:42:02 +0000

    Fix

--- R/shapeit.R
+++ R/shapeit.R
@@ -35,69 +35,7 @@ phase_shapeit=function(
             append_env(to=.this.env,from=.env)
             set_main(.env=.this.env)
 
-            phase_chr_shapeit=function(
-                bin_bcftools=build_default_tool_binary_list()$bin_bcftools,
-                bin_bgzip=build_default_tool_binary_list()$bin_bgzip,
-                bin_tabix=build_default_tool_binary_list()$bin_tabix,
-                bin_shapeit=build_default_tool_binary_list()$bin_shapeit,
-                ref_panel=build_default_reference_list()$HG19$phasing$G1000$new$vcf,
-                gmap=build_default_reference_list()$HG19$phasing$G1000$new$gmap,
-                vcf=NULL,
-                chr=c(1:22,"X"),
-                scaffold=NULL,
-                output_name=NULL,
-                ...
-            ){
-
-                    run_main=function(.env){
-
-                        .this.env=environment()
-                        append_env(to=.this.env,from=.env)
-                        set_main(.env=.this.env)
-
-                        output_name=paste0(input_id,".",chr)
-
-                        .main$steps[[fn_id]]<-.this.env
-                        .main.step=.main$steps[[fn_id]]
-
-                        .main.step$out_files$phased_vcf=paste0(out_file_dir,"/",input_id,".phased.vcf")
-
-
-                        if(!is.null(ref_panel)){
-                            add=paste0(" --reference ",ref_panel[grepl(paste0("chr",input,"."),ref_panel)]) 
-                        }
-
-                        if(!is.null(scaffold)){
-                            add=paste0(" --scaffold ",scaffold)
-                        }
-
-                        .main$exec_code=paste(
-                            bin_shapeit," --input ",.main.step$out_files$bgzip_vcf,
-                            " --map ",gmap[grepl(paste0("chr",input,"."),gmap)],
-                            add,
-                            " --region ",input,
-                            " --thread ",threads,
-                            " --output ",.main.step$out_files$phased_vcf,
-                            " --sequencing"
-                        )
-
-                        run_job(.env=.this.env)
-
-                        .env$.main <- .main
-                        
-                    }
-
-                    .base.env=environment()
-                    list2env(list(...),envir=.base.env)
-                    set_env_vars(
-                    .env= .base.env,
-                    vars="chr"
-                    )
-                
-                    launch(.env=.base.env)
-
-            }
-
+        
             .main$steps[[fn_id]]<-.this.env
             .main.step=.main$steps[[fn_id]]
 
@@ -185,4 +123,82 @@ phase_shapeit=function(
 
 }
 
+
+
+
+#' Strelka wrapper for SNV variant calling
+#'
+#' This function wraps the STRELKA functions for variant calling
+#' 
+#' @param bin_shapeit Path to SHAPEIT binary file
+#' @param haplotype Path to haplotype information from 1000G
+#' @param legend Path to legend information from 1000G
+#' @param map Path to map information from 1000G
+#' @param vcf Path to VCF file
+#' @export
+
+
+
+phase_chr_shapeit=function(
+            bin_bcftools=build_default_tool_binary_list()$bin_bcftools,
+            bin_bgzip=build_default_tool_binary_list()$bin_bgzip,
+            bin_tabix=build_default_tool_binary_list()$bin_tabix,
+            bin_shapeit=build_default_tool_binary_list()$bin_shapeit,
+            ref_panel=build_default_reference_list()$HG19$phasing$G1000$new$vcf,
+            gmap=build_default_reference_list()$HG19$phasing$G1000$new$gmap,
+            vcf=NULL,
+            chr=c(1:22,"X"),
+            scaffold=NULL,
+            output_name=NULL,
+            ...
+        ){
+
+                    run_main=function(.env){
+
+                        .this.env=environment()
+                        append_env(to=.this.env,from=.env)
+                        set_main(.env=.this.env)
+
+                        output_name=paste0(input_id,".",chr)
+
+                        .main$steps[[fn_id]]<-.this.env
+                        .main.step=.main$steps[[fn_id]]
+
+                        .main.step$out_files$phased_vcf=paste0(out_file_dir,"/",input_id,".phased.vcf")
+
+
+                        if(!is.null(ref_panel)){
+                            add=paste0(" --reference ",ref_panel[grepl(paste0("chr",input,"."),ref_panel)]) 
+                        }
+
+                        if(!is.null(scaffold)){
+                            add=paste0(" --scaffold ",scaffold)
+                        }
+
+                        .main$exec_code=paste(
+                            bin_shapeit," --input ",.main.step$out_files$bgzip_vcf,
+                            " --map ",gmap[grepl(paste0("chr",input,"."),gmap)],
+                            add,
+                            " --region ",input,
+                            " --thread ",threads,
+                            " --output ",.main.step$out_files$phased_vcf,
+                            " --sequencing"
+                        )
+
+                        run_job(.env=.this.env)
+
+                        .env$.main <- .main
+                        
+                    }
+
+                    .base.env=environment()
+                    list2env(list(...),envir=.base.env)
+                    set_env_vars(
+                    .env= .base.env,
+                    vars="chr"
+                    )
+                
+                    launch(.env=.base.env)
+
+}
 
