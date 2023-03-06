46387c6879481e8717928907b3ed08ed76d96c1d
TearsWillFall <osvaldas.vainauskas@gmail.com>
2023-03-06 10:13:18 +0000

    Fix

--- R/phasing.R
+++ R/phasing.R
@@ -8,6 +8,7 @@ plot_phased=function(
     format="png"
 ){
     
+    library(patchwork)
     out_file=paste0(get_file_name(normal),".",format)
     normal=read.table(normal,sep="\t",header=TRUE)
     tumours=lapply(tumour,read.table,sep="\t",header=TRUE)
@@ -18,6 +19,18 @@ plot_phased=function(
     tumours_long$gt_col=ifelse(tumours_long$gt=="1|0","blue","yellow")
     tumours_long_filt=tumours_long %>% 
     dplyr::filter(depth>=normal_cov,af<=normal_af&(1-normal_af)>=af)
+
+    tumours_wider_cov=tumours %>% tidyr::pivot_wider(id_cols=c(chrom,pos),names_from=id,values_from=log2)
+    tumours_wider_cov=dplyr::left_join(normal %>% dplyr::select(chrom,pos,ref,alt,gt,af,depth),tumours_wider)
+    tumours_long_cov=tumours_wider_cov %>% tidyr::pivot_longer(cols=!chrom:depth)
+    tumours_long_cov$gt_col=ifelse(tumours_long_cov$gt=="1|0","blue","yellow")
+    tumours_long_cov_filt=tumours_long_cov %>% 
+    dplyr::filter(depth>=normal_cov,af<=normal_af&(1-normal_af)>=af)
+
+
+
+
+
     p1<-ggplot(tumours_long_filt %>% dplyr::arrange(pos) %>% 
     dplyr::mutate(inorder=as.numeric(as.factor(pos))),aes(inorder,value,col=gt_col))
     p1<-p1+geom_hline(aes(yintercept=0.5),linetype="longdash")+
@@ -27,11 +40,26 @@ plot_phased=function(
     scale_colour_identity()+
     theme_bw()+scale_y_continuous(limits=c(0,1),expand=c(0,0))+
     facet_grid(name~"")
+
+
+
+    p2<-ggplot(tumours_long_cov_filt %>% dplyr::arrange(pos) %>% 
+    dplyr::mutate(inorder=as.numeric(as.factor(pos))),aes(inorder,value/log2,col=gt_col))
+    p1<-p1+geom_hline(aes(yintercept=0.5),linetype="longdash")+
+    geom_hline(aes(yintercept=0.25),alpha=0.5,linetype="longdash")+
+    geom_hline(aes(yintercept=0.75),alpha=0.5,linetype="longdash")
+    p1<-p1+geom_point(size=0.1)+geom_smooth(se=FALSE)+
+    scale_colour_identity()+
+    theme_bw()+scale_y_continuous(limits=c(0,1),expand=c(0,0))+
+    facet_grid(name~"")
+
+
+
     if(save){
          ggsave(
              out_file,
-            plot=p1,height=length(tumour)*600,
-            width=1200,units="px"
+            plot=p1|p2,height=length(tumour)*600,
+            width=2*1200,units="px"
         )
     }
    
