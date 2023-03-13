cc90ca618e07d95f35a0d173fc74c2b9db36f51f
TearsWillFall <osvaldas.vainauskas@gmail.com>
2023-03-13 10:34:57 +0000

    Fix

--- R/coverage.R
+++ R/coverage.R
@@ -3,8 +3,8 @@
 
 fraction_genome_altered=function(
     cnr=NULL,
-    gain=seq(0,0.3,by=0.05),
-    loss=seq(-0.6,0,by=0.05),
+    gain=seq(0,1,by=0.01),
+    loss=seq(-1,0,by=0.01),
     threads=1
 ){
 
@@ -12,6 +12,7 @@ fraction_genome_altered=function(
     all_info=mclapply_os(loss,FUN=function(x){
         info=lapply(gain,FUN=function(y){
             dat_tmp=dat
+            dat_tmp$width=dat_tmp$end-dat_tmp$start
             dat_tmp$TYPE=ifelse(dat_tmp$log2>=y,"GAIN",
                 ifelse(dat_tmp$log2<=x,"LOSS","WT"
             ))
@@ -22,15 +23,24 @@ fraction_genome_altered=function(
                     N=dplyr::n(),
                     N_target=length(log2[gene!="Antitarget"]),
                     N_antitarget=length(log2[gene=="Antitarget"]),
+                    G_size=sum(width),
+                    G_size_target=sum(width[gene!="Antitarget"]),
+                    G_size_antitarget=sum(width[gene!="Antitarget"]),
                     weight=median(weight),
-                    weight_target=median(weight[gene!="Antitarget"]),
-                    weight_antitarget=median(weight[gene=="Antitarget"])
+                    weight_target=median(weight[gene!="Antitarget"],na.rm=TRUE),
+                    weight_antitarget=median(weight[gene=="Antitarget"],na.rm=TRUE)
                 ) %>% 
                 dplyr::ungroup() %>% 
                 dplyr::mutate(
                     FRACTION=N/sum(N),
                     FRACTION_target=N_target/sum(N_target),
-                    FRACTION_antitarget=N_antitarget/sum(N_antitarget)
+                    FRACTION_antitarget=N_antitarget/sum(N_antitarget),
+                    FRACTION_genome=G_size/sum(G_size),
+                    FRACTION_genome_target=G_size_target/sum(G_size_target),
+                    FRACTION_genome_antitarget=G_size_antitarget/sum(G_size_antitarget),
+                    T_G_size=sum(G_size),
+                    T_G_size_target=sum(G_size_target),
+                    T_G_size_antitarget=sum(G_size_antitarget)
                 )
 
             dat_rslt$gain=y
