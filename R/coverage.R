5df08d400237acb39b2f8832e9a2b1f78efdbf20
TearsWillFall <osvaldas.vainauskas@gmail.com>
2023-03-20 12:09:08 +0000

    Fix

--- R/coverage.R
+++ R/coverage.R
@@ -1,7 +1,7 @@
 
 #' @export
 
-fraction_genome_altered=function(
+generate_pga=function(
     cnr=NULL,
     gain=seq(0,1,by=0.01),
     loss=seq(-1,0,by=0.01),
@@ -99,5 +99,70 @@ fraction_genome_altered=function(
     launch(.env=.base.env)
 
 
+
+}
+
+
+
+#' @export
+
+extract_pga=function(tumour=NULL,normal=NULL){
+
+    tumour=read.table(tumour,sep="\t",header=TRUE)
+    normal=read.table(normal,sep="\t",header=TRUE)
+    tumour=dplyr::left_join(
+        tumour,normal,
+        by=c("TYPE"="TYPE","loss"="loss","gain"="gain"),
+        suffix=c(".tumour",".normal")
+    )
+    tumour=tumour %>% 
+        dlyr::mutate(
+            FRACTION=FRACTION.tumour-FRACTION.normal,
+            FRACTION_target=FRACTION_target.tumour-FRACTION_target.normal,
+            FRACTION_antitarget=FRACTION_antitarget.tumour-FRACTION_antitarget.normal,
+            FRACTION_genome=FRACTION_genome.tumour-FRACTION_genome.normal,
+            FRACTION_genome_target=FRACTION_genome_target.tumour-FRACTION_genome_target.normal,
+            FRACTION_genome_antitarget=FRACTION_genome_antitarget.tumour-FRACTION_genome_antitarget.normal
+    )
+            bin_all=tumour %>% dplyr::group_by(TYPE) %>% 
+                dplyr::mutate(BASE=FRACTION[gain==0&loss==0]) %>% 
+                dplyr::filter(FRACTION==max(FRACTION)) %>% 
+                dplyr::select(TYPE,loss,gain,BASE,FRACTION) %>% 
+                dplyr::rename_with("FRACTION",starts_with("FRACTION")) %>%
+                dplyr::mutate(PGA=sum(FRACTION),method="bin",source="all")
+            bin_target=tumour %>% dplyr::group_by(TYPE)%>% 
+                dplyr::mutate(BASE=FRACTION_target[gain==0&loss==0]) %>% 
+                dplyr::filter(FRACTION_target==max(FRACTION_target)) %>% 
+                dplyr::select(TYPE,loss,gain,BASE,FRACTION_target)%>% 
+                dplyr::rename_with("FRACTION",starts_with("FRACTION"))
+                dplyr::mutate(PGA=sum(FRACTION),method="bin",source="target")
+            bin_antitarget=tumour %>% dplyr::group_by(TYPE)%>% 
+                dplyr::mutate(BASE=FRACTION_antitarget[gain==0&loss==0]) %>% 
+                dplyr::filter(FRACTION_antitarget==max(FRACTION_antitarget)) %>% 
+                dplyr::select(TYPE,loss,gain,BASE,FRACTION_antitarget)%>% 
+                dplyr::rename_with("FRACTION",starts_with("FRACTION")) %>%
+                dplyr::mutate(PGA=sum(FRACTION),method="bin",source="antitarget")
+            base_all=tumour %>% dplyr::group_by(TYPE)%>% 
+                dplyr::mutate(BASE=FRACTION_genome[gain==0&loss==0]) %>% 
+                dplyr::filter(FRACTION_genome==max(FRACTION_genome))%>% 
+                dplyr::select(TYPE,loss,gain,BASE,FRACTION_genome)%>% 
+                dplyr::rename_with("FRACTION",starts_with("FRACTION")) %>%
+                dplyr::mutate(PGA=sum(FRACTION),method="base",source="all")
+            base_target=tumour %>% dplyr::group_by(TYPE)%>% 
+                dplyr::mutate(BASE=FRACTION_genome_target[gain==0&loss==0]) %>% 
+                dplyr::filter(FRACTION_genome_target==max(FRACTION_genome_target))%>% 
+                dplyr::select(TYPE,loss,gain,BASE,FRACTION_genome_target)%>% 
+                dplyr::rename_with("FRACTION",starts_with("FRACTION")) %>%
+                dplyr::mutate(PGA=sum(FRACTION),method="base",source="target")
+            base_antitarget=tumour %>% 
+                dplyr::group_by(TYPE)%>% 
+                dplyr::mutate(BASE=FRACTION_genome_antitarget[gain==0&loss==0]) %>% 
+                dplyr::filter(FRACTION_genome_antitarget==max(FRACTION_genome_antitarget))%>% 
+                dplyr::select(TYPE,loss,gain,BASE,FRACTION_genome_antitarget)%>% 
+                dplyr::rename_with("FRACTION",starts_with("FRACTION")) %>% 
+                dplyr::mutate(PGA=sum(FRACTION),method="base",source="antitarget")
+
+            dat_summ=dplyr::bind_rows(bin_all,bin_target,bin_antitarget,base_all,base_target,base_antitarget)
+            return(dat_summ)
 
 }
