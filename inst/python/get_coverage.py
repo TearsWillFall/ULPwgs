a5bb61e0d0170695a08349271c69789aa57a2496
TearsWillFall <osvaldas.vainauskas@gmail.com>
2023-07-26 12:50:49 +0100

    Fix

--- inst/python/get_coverage.py
+++ inst/python/get_coverage.py
@@ -7,61 +7,65 @@ import numpy as np
 import multiprocessing as mp
 from datetime import datetime
 
-def get_coverage(gpos:None,bamfile:None,output:None,force:False,id:None):
 
-   chr=None
-   start=None
-   end=None
-   ref="."
-   alt="."
-   gt="."
-   gid="."
-   af=0
+def divide_chunks(lst, n):
+    for i in range(0, len(lst), n): 
+        yield lst[i:i + n]
 
+def get_coverage(gps:None,bamfile:None,output:None,force:False,id:None):
    bamfile = pysam.AlignmentFile(bam, "rb")
    if gpos!=None:
-         chr=gpos[0]
-         if gpos[1]!=None:
-            start=int(gpos[1])-1
-            end=int(gpos[1])
-         if gpos[2]!=None:
-            ref=gpos[2]
-         if gpos[3]!=None:
-            alt=gpos[3]
-         if gpos[4]!=None:
-            gt=gpos[4]
-         if gpos[5]!=None:
-            gid=gpos[5]
+         for gpos in gps:
+            chr=None
+            start=None
+            end=None
+            ref="."
+            alt="."
+            gt="."
+            gid="."
+            af=0
+            chr=gpos[0]
+            if gpos[1]!=None:
+               start=int(gpos[1])-1
+               end=int(gpos[1])
+            if gpos[2]!=None:
+               ref=gpos[2]
+            if gpos[3]!=None:
+               alt=gpos[3]
+            if gpos[4]!=None:
+               gt=gpos[4]
+            if gpos[5]!=None:
+               gid=gpos[5]
    
-   cov=bamfile.count_coverage(
-      contig=chr,
-      start=start,
-      stop=end,
-      quality_threshold=qt
-   )
+            cov=bamfile.count_coverage(
+               contig=chr,
+               start=start,
+               stop=end,
+               quality_threshold=qt
+            )
 
-   baseCount={"A":sum(cov[0]),"C":sum(cov[1]),"G":sum(cov[2]),"T":sum(cov[3])}
-   bTotal=baseCount["A"]+baseCount["C"]+baseCount["G"]+baseCount["T"]
-   if not ref=="." and not alt==".":
-      if (baseCount[ref]+baseCount[alt])>0:
-            af=baseCount[alt]/(baseCount[ref]+baseCount[alt])
-      
-   if output!=None:
-      if not os.path.exists(output) or force:
-         f=open(output,"a")
-         if id!=None:
-            f.write("\t".join(map(str,[chr,end,ref,alt,gt,gid,baseCount["A"],baseCount["C"],baseCount["G"],baseCount["T"],bTotal,af,id]))+"\n")
-         else:
-            f.write("\t".join(map(str,[chr,end,ref,alt,gt,gid,baseCount["A"],baseCount["C"],baseCount["G"],baseCount["T"],bTotal,af]))+"\n")
-         f.close()
-      else:
-         raise FileExistsError
-   else:
-      if id!=None:
-         print(chr,end,ref,alt,gt,gid,baseCount["A"],baseCount["C"],baseCount["G"],baseCount["T"],bTotal,af,id,sep="\t")
-      else:
-         print(chr,end,ref,alt,gt,gid,baseCount["A"],baseCount["C"],baseCount["G"],baseCount["T"],bTotal,af,sep="\t")
- 
+            baseCount={"A":sum(cov[0]),"C":sum(cov[1]),"G":sum(cov[2]),"T":sum(cov[3])}
+            bTotal=baseCount["A"]+baseCount["C"]+baseCount["G"]+baseCount["T"]
+            if not ref=="." and not alt==".":
+               if (baseCount[ref]+baseCount[alt])>0:
+                     af=baseCount[alt]/(baseCount[ref]+baseCount[alt])
+               
+            if output!=None:
+               if not os.path.exists(output) or force:
+                  f=open(output,"a")
+                  if id!=None:
+                     f.write("\t".join(map(str,[chr,end,ref,alt,gt,gid,baseCount["A"],baseCount["C"],baseCount["G"],baseCount["T"],bTotal,af,id]))+"\n")
+                  else:
+                     f.write("\t".join(map(str,[chr,end,ref,alt,gt,gid,baseCount["A"],baseCount["C"],baseCount["G"],baseCount["T"],bTotal,af]))+"\n")
+                  f.close()
+               else:
+                  raise FileExistsError
+            else:
+               if id!=None:
+                  print(chr,end,ref,alt,gt,gid,baseCount["A"],baseCount["C"],baseCount["G"],baseCount["T"],bTotal,af,id,sep="\t")
+               else:
+                  print(chr,end,ref,alt,gt,gid,baseCount["A"],baseCount["C"],baseCount["G"],baseCount["T"],bTotal,af,sep="\t")
+         
 if __name__ == "__main__":
    bam = None
    gpos=None
@@ -112,7 +116,7 @@ if __name__ == "__main__":
       if os.path.exists(gpos):
          with open(gpos) as f:
             for line in f:
-                  gposcontent.append(line.strip().split())
+                  gposcontent.append(list(line.strip().split()))
       else:
          raise ValueError("Path to genomic position file not found")
    elif region[0]!=None:
@@ -120,7 +124,9 @@ if __name__ == "__main__":
    else:
       raise ValueError("No genomic position was provided")
 
-   jobs=len(gposcontent)
+   chunk_size=int(len(gposcontent)/threads)
+   chunks=list(divide_chunks(gposcontent,chunk_size))
+   jobs=len(chunks)
 
    if output!=None:
       if not os.path.exists(output) or force:
@@ -138,11 +144,12 @@ if __name__ == "__main__":
       else:
          print("chrom\tpos\tref\talt\tgt\tgid\tA\tC\tG\tT\tdepth\taf")
 
+
    if jobs==1 or threads==1:
-      list(map(get_coverage,gposcontent,[bam]*jobs,[output]*jobs,[True]*jobs,[id]*jobs))
+      list(map(get_coverage,gposcontent,[bam],[output],[True],[id]))
    elif jobs>1:
       with mp.Pool(threads) as thread:
-         list(thread.starmap(get_coverage,zip(gposcontent,[bam]*jobs,[output]*jobs,[True]*jobs,[id]*jobs)))
+         list(thread.starmap(get_coverage,zip(chunks,[bam]*threads,[output]*threads,[True]*threads,[id]*threads)))
    end=datetime.now()
   
    if verbose:
@@ -150,3 +157,4 @@ if __name__ == "__main__":
       print("• Run Time: ", end-start)
       print("• Jobs: ", jobs)
       print("• Threads: ", threads)
+
