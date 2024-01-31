59f5c38e300c257defb6346994476f9220062fd8
TearsWillFall <osvaldas.vainauskas@gmail.com>
2024-01-31 12:30:21 +0000

    Fix

--- R/vcf.R
+++ R/vcf.R
@@ -17,8 +17,8 @@ read_vcf=function(vcf=NULL,sep="\t",threads=1){
     stop("vcf arguments is of type NULL")
   }else if(check_if_compressed(vcf)){
       body=data.table::fread(cmd=paste0("gunzip -c ",vcf," | grep -v ^## "),nThread=threads,colClasses="character")
-      header=system(paste0("grep ^# ",vcf),intern=TRUE)
-  }else{
+      header=system(paste0("gunzip -c ",vcf, "| grep ^#" ),intern=TRUE)
+  }else
       body=data.table::fread(cmd=paste0("grep -v ^## ",vcf),nThread=threads,colClasses="character")
       header=system(paste0("grep ^# ",vcf),intern=TRUE)
   }
