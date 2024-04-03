102e41124f45db5a3367916b0c528b6a5f3f7ca8
TearsWillFall <osvaldas.vainauskas@gmail.com>
2024-04-03 14:41:45 +0100

    Fix

--- R/batch_constructor.R
+++ R/batch_constructor.R
@@ -851,10 +851,13 @@ run=function(.env){
 set_base_env=function(){
         .base.env=parent.frame()
         ### ADD OTHER VARIABLES TO BASE ENV
-        list2env(list(.base.env$`...`),envir=.base.env)
+        list2env(list(.base.env$...),envir=.base.env)
         ## CREATE FUNCTION VARIABLE NAMES
         .base.env$fn_vars=names(.base.env)
 }
+
+
+
 
 
 #' Set steps enviroment for use
