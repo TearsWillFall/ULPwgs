d40da1ebfc8898a69bd5ba6d1d6747608f734e46
TearsWillFall <osvaldas.vainauskas@gmail.com>
2024-10-08 15:37:11 +0100

    Fix

--- R/bcftools.R
+++ R/bcftools.R
@@ -308,7 +308,8 @@ mpileup_bcftools<-function(
           " -Ou  -a FORMAT/AD,FORMAT/ADF,FORMAT/ADR,FORMAT/DP,FORMAT/SP,INFO/AD,INFO/ADF,INFO/ADR |",  
           bin_bcftools, 
           " call -mv -Ou |",
-          bin_bcftools, " +fill-tags -- -t FORMAT/VAF -o ",  
+          bin_bcftools, " +fill-tags -- -t FORMAT/VAF -Ou |",
+          bin_bcftools, " norm -m -any -o ",
           .main$out_files$mpileup_vcf
         )
         run_job(.env=.this.env)
