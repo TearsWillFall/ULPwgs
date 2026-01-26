45cfad9256c830bdd51fce6df07980a60125b67b
TearsWillFall <o.vainauskas@ucl.ac.uk>
2026-01-26 16:48:58 +0000

    Fix

--- R/batch_constructor.R
+++ R/batch_constructor.R
@@ -897,13 +897,9 @@ set_main_env=function(.env){
           .env$self.envs <- .this.env
 
           ### Check if list of lists
-          if(n_inputs==1&is.list(inputs)){
-            input<-inputs[[1]]
-          }else{
-            input<-inputs[n]
-          }
 
-    
+       
+          input<-input[[n]]
           n_inputs<- 1
           input_id<-inputs_id[n]
           input_ext<-inputs_ext[n]
