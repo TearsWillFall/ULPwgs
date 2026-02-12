1ba9e92cada8750f9d5c41337b93097d6d06df16
TearsWillFall <o.vainauskas@ucl.ac.uk>
2026-02-12 12:33:24 +0000

    Fix

--- R/preprocess.R
+++ R/preprocess.R
@@ -2175,7 +2175,7 @@ preprocess_default=function(
                         )
                     )
 
-                .this.step=.main.step$steps$new_recal_gatk
+                .this.step=.main.step$steps$new_recal_gatk2
                 .main.step$out_files$recal=.this.step$out_files$recal
             }
 
